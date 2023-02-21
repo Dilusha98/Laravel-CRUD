d4baba0df1941847f93c5cb02a01310166577219
Dilusha98 <89926492+Dilusha98@users.noreply.github.com>
2023-02-21 17:30:16 +0530

    second

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -74,7 +74,9 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"
         integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script>
-        function getFormValue(e) {
+
+        $(document).ready(function () {
+            function getFormValue(e) {
             e.preventDefault();
             var data = {
                 'name': $('name').val(),
@@ -101,7 +103,12 @@
 
         }
 
-        $('.deleteBtn').click(function(e) {
+        });
+
+        
+        $(document).ready(function () {
+            
+            $('.deleteBtn').click(function(e) {
             e.preventDefault();
             var id = $(this).data('id');
             console.log(id);
@@ -123,7 +130,10 @@
             window.location.reload();
         });
 
-        $('.updateBtn').click(function(e) {
+        });
+
+        $(document).ready(function () {
+            $('.updateBtn').click(function(e) {
             e.preventDefault();
 
             var id = $(this).data('id');
@@ -175,6 +185,8 @@
                 window.location.reload();
 
             });
+        });
+
         });
     </script>
 @endsection
